f12ee53f1ec1c14c54d4331b3eaf4e533d6f3742
Jason5521 <jasonb185@deltalearns.ca>
2026-04-01 10:28:33 -0700

    _____________________

--- task1.py
+++ task1.py
@@ -9,7 +9,25 @@ Execute the function of the letter they use.
 """
 
 def A():
+    print("Hello")
 
 def B():
+    print("How are you")
 
 def C():
+    print("hi")
+
+
+# ask the user to enter a letter A - C
+# check the letter to see what it is
+# if A, then A()
+# if B, then B()
+# if C, then C()
+
+Letter = input("Enter a letter from A to C: ")
+if Letter == "A":
+    A()
+elif Letter == "B":
+    B()
+elif Letter == "C":
+    C()
--- task2.py
+++ task2.py
@@ -10,3 +10,15 @@ plays the game
 This will be silimar to something you have already done, but in this task you 
 are breaking the code up into different sections to make each a function.
 """
+def title():
+    print("Welcome to the number guessing game!")
+    print("In this game, you must guess the number i am thinking of.")
+    print("The number cannot be above 10.")
+    print("To play the game, you must press a number key on your [bold]keybaord[/bold] to guess a number.")
+    print("Then, you must press the [bold]ENTER[/bold] key to present your guess,")
+    print("and I'll tell you if you got it right or wrong.")
+    print("Oh, do not type out the numbers using the letter keys! just press the number keys.")
+
+def game():
+    guess = int("Very well, please present your guess now.")
+    
